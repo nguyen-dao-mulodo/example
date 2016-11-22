bb5f0d542203f7c5e51b49fcd52cbd2f5fb2f42f
Admin <admin@Admins-MacBook-Pro.local>
2016-11-22 15:09:49 +0700

    11vs22

--- f1.php
+++ f1.php
@@ -1,6 +1,3 @@
 <?php
 echo "1";echo "1";echo "1";echo "1";
 echo "1";echo "1";echo "1";echo "1";
-echo "1";echo "1";echo "1";echo "1";
-echo "1";echo "1";echo "1";echo "1";
-echo "1";echo "1";echo "1";echo "1";
